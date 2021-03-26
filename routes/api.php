b22f754ac329b55030370bd1aeb778de4999e878
Iyathurai Iyngaran <dev@iyngaran.info>
2021-03-26 21:50:03 +0530

    wip

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use Iyngaran\User\Http\Controllers\Api\RegisterController;
 use Iyngaran\User\Http\Controllers\Api\LoginController;
-use Iyngaran\User\Http\Controllers\Api\UserController;
+use Iyngaran\User\Http\Controllers\Api\Secure\UserController;
 
 Route::post('/register', RegisterController::class);
 Route::post('/login', LoginController::class);
 
-Route::resource('/user', UserController::class)->except([
-    'create', 'edit'
-])->middleware(['auth:sanctum']);
+Route::resource('/user', UserController::class)->only([
+    'index',
+]);
+
+Route::group(
+    ['prefix' => 'secure', 'as' => 'secure.'],
+    function () {
+        Route::resource('/user', UserController::class)->except([
+            'create',
+            'edit'
+        ])->middleware(['auth:sanctum']);
+    });
 
 
--- src/Http/Controllers/Api/UserController.php
+++ src/Http/Controllers/Api/UserController.php
@@ -3,18 +3,10 @@
 
 namespace Iyngaran\User\Http\Controllers\Api;
 
-use Illuminate\Http\JsonResponse;
+
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Routing\Controller;
-use Iyngaran\User\Actions\CreateAction;
-use Iyngaran\User\Actions\DeleteAction;
-use Iyngaran\User\Actions\UpdateAction;
-use Iyngaran\User\DTO\UserData;
-use Iyngaran\User\Http\Requests\DestroyRequest;
 use Iyngaran\User\Http\Requests\IndexRequest;
-use Iyngaran\User\Http\Requests\ShowRequest;
-use Iyngaran\User\Http\Requests\StoreRequest;
-use Iyngaran\User\Http\Requests\UpdateRequest;
 use Iyngaran\User\Http\Resources\UserResource;
 use Iyngaran\User\Repositories\UserRepositoryInterface;
 
@@ -25,29 +17,4 @@ class UserController extends Controller
         return UserResource::collection($user->search($request));
     }
 
-    public function store(StoreRequest $request): JsonResponse
-    {
-        return response()->json(
-            new UserResource((new CreateAction())->execute(UserData::formRequest($request))),
-            201
-        );
-    }
-
-    public function show(ShowRequest $request, UserRepositoryInterface $user, $id): JsonResponse
-    {
-        return response()->json(new UserResource($user->find($id)));
-    }
-
-    public function update(UpdateRequest $request, UserRepositoryInterface $user, $id): JsonResponse
-    {
-        return response()->json(
-            new UserResource((new UpdateAction())->execute(UserData::formRequest($request), $user->find($id))),
-            200
-        );
-    }
-
-    public function destroy(DestroyRequest $request, UserRepositoryInterface $user, $id): JsonResponse
-    {
-        return response()->json((new DeleteAction())->execute($user->find($id)), 204);
-    }
 }
